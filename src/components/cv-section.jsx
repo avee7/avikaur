0e922370a3d26d103d7ccd9107696c53d0121b69
avee7 <kauravee101@gmail.com>
2024-05-10 17:04:49 +0100

    Resume added

--- src/components/cv-section.jsx
+++ src/components/cv-section.jsx
@@ -1,22 +1,20 @@
-import React from "react"
-import { Box, Heading, VStack, Container, Button } from '@chakra-ui/react';
-
-
+import React from 'react';
+import { Container, VStack, Heading, Button } from '@chakra-ui/react';
 
 const CVSection = () => (
-    <Container maxW="1300px" pb="14rem" pt="2rem">
-      
-
-      
-        <VStack justifyContent="center" alignItems="center">
-          <Heading size="xl" mb={14}>Dive Deeper into My Professional Journey</Heading>
-          <Button variant="outline">Download Resume</Button>
-        </VStack>
-
-    
-    </Container>
-    
-
+  <Container maxW="1300px" pb="14rem" pt="2rem">
+    <VStack justifyContent="center" alignItems="center">
+      <Heading size="xl" mb={14}>Dive Deeper into My Professional Journey</Heading>
+      <Button
+        variant="outline"
+        as="a"
+        href="/resume.pdf" 
+        download="AvinashKaur.pdf" 
+      >
+        Download Resume
+      </Button>
+    </VStack>
+  </Container>
 );
 
 export default CVSection;
