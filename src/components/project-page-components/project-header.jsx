8e84a6264c0dc7ea8273ca626f134200b2f286bf
avee7 <kauravee101@gmail.com>
2024-05-24 04:35:48 +0100

    Adding responsive behaviour on project page

--- src/components/project-page-components/project-header.jsx
+++ src/components/project-page-components/project-header.jsx
@@ -4,15 +4,17 @@ import { Stack, Box, Text, Heading, Image, Container, Flex, GridItem } from '@ch
 
 const ProjectHeader = ({ title, subtitle, bg, img }) => (
   <Box bg={bg} >
- <Flex maxW="1300px" mx="auto" pt="11rem" pb="22rem" alignItems="center" justifyItems="center" direction="column" width="100%" position="relative">
-      
-          <Heading size="3xl">{title}</Heading>
-          <Text size="xl" mt={2}>{subtitle} </Text>
-          <Image src={img} borderRadius="12px 12px 0 0" width="800px" height="auto" position="absolute" left="50%" transform="translateX(-50%)" bottom="0" boxShadow="0px 8px 16px rgba(0, 0, 0, 0.2)" />
-      
-  </Flex>
+    <Container maxW="1300px" >
+      <Flex mx="auto" pt="11rem" pb="22rem" alignItems="center" justifyItems="center" direction="column" width="100%" position="relative">
+
+        <Heading size="3xl" textAlign="center">{title}</Heading>
+        <Text size="2xl" mt={2}>{subtitle} </Text>
+        <Image src={img} borderRadius="12px 12px 0 0" width="800px" height="auto" position="absolute" left="50%" transform="translateX(-50%)" bottom="0" boxShadow="0px 8px 16px rgba(0, 0, 0, 0.2)" />
+
+      </Flex>
+    </Container>
   </Box>
- 
+
 );
 
 export default ProjectHeader;
