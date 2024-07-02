92e5dd1c1ff1f3d81e0e68551360c8c3fef122c9
avee7 <kauravee101@gmail.com>
2024-07-02 17:08:02 +0100

    Animation to project page WIP

--- src/components/project-page-components/new-home-page-preview.jsx
+++ src/components/project-page-components/new-home-page-preview.jsx
@@ -1,62 +1,76 @@
 import React from "react"
-import { Box, Text, Heading, Image, Flex, VStack } from '@chakra-ui/react'
+import { Box, Text, Heading, Image, Flex, VStack, useBreakpointValue } from '@chakra-ui/react'
 import HomePageVideo from "../../static/Mendeley-homepage.mp4"
 import MendeleyTrust from '../../images/mendeley-trust.png';
 import MendeleyEngagement from '../../images/mendeley-engagement.png';
 import MendeleyClarity from '../../images/mendeley-clarity.png';
 
 
-const NewHomePagePreview = () => (
-  <Box>
-     <Flex gap="3rem" pt={{ base: "6rem",  md: "10rem"}} width="100%" justifyContent="space-between">
-          <Heading size={{base: 'xl', md: '2xl'}}>The new home page experience</Heading>
-        </Flex>
-        <Flex mt={{ base: "2rem",  md: "4rem"}} gap={8} direction={{base: 'column', md: 'row'}}>
-          <Box width="full" maxW="900px" mx="auto">
-            <video controls width="100%" autoPlay muted style={{ borderRadius: '8px' }}>
-              <source src={HomePageVideo} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </Box>
-          <Box>
-            <Text size="lg" mb="1rem">KEY RESULTS</Text>
-            <VStack gap={3} alignItems="flex-start">
-              <Box p="1.5rem" borderRadius="8px" bg="#1a1a1a">
-                <Heading size="sm" mb="4px">Content Engagement</Heading>
-                <Flex direction="row" gap={2} alignItems="center">
-                  <Image src={MendeleyEngagement} maxW="50%" />
-                  <Text size="md">4.5 / 5</Text>
-                </Flex>
-              </Box>
-              <Box p="1.5rem" borderRadius="8px" bg="#1a1a1a">
-                <Heading size="sm" mb="4px">Perception of Trust</Heading>
-                <Flex direction="row" gap={2} alignItems="center">
-                  <Image src={MendeleyTrust} maxW="50%" />
-                  <Text size="md">4.4 / 5</Text>
-                </Flex>
-              </Box>
-              <Box p="1.5rem" borderRadius="8px" bg="#1a1a1a">
-                <Heading size="sm" mb="4px">Content Clarity</Heading>
-                <Flex direction="row" gap={2} alignItems="center">
-                  <Image src={MendeleyClarity} maxW="50%" />
-                  <Text size="md">4.5 / 5</Text>
-                </Flex>
-              </Box>
-            </VStack>
-            <Text size="xs" mt={2}>
-              These are pre-launch metrics from qual and quant testing. Post-launch metrics are pending as the site will be launched in late 2024
-            </Text>
+const NewHomePagePreview = () => {
+  const isLgOrLarger = useBreakpointValue({ base: false, lg: true });
+  return(
+    <Box>
+      <Flex gap="3rem" pt={{ base: "6rem", md: "10rem" }} width="100%" justifyContent="space-between">
+        <Heading size={{ base: 'xl', md: '2xl' }}>The new home page experience</Heading>
+      </Flex>
+      <Box position="relative" mt={{ base: "2rem", md: "4rem" }}>
+        <Box maxW="1100px" bg="rgba(90, 93, 159, 0.6)" p={{base: '1rem', lg: '4rem'}} style={{ borderRadius: '8px' }}>
+          <video controls width="100%" autoPlay muted loop style={{ borderRadius: '8px' }} mx="auto">
+            <source src={HomePageVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          
+        </Box>
+        <Text size="xs" mt={2}>
+            These are pre-launch metrics from qual and quant testing. Post-launch metrics are pending as the site will be launched in late 2024.
+          </Text>
+        <Box
+          position={isLgOrLarger ? "absolute" : "static"}
+          top={isLgOrLarger ? { base: '2rem', md: '5rem' } : "auto"}
+          right={isLgOrLarger ? { base: '1rem', md: '-2rem' } : "auto"}
+          
+          p={{base: '1rem', md: '2rem'}}
+          borderRadius="8px"
+          maxW="360px"
+          zIndex={1}
+          mt={!isLgOrLarger ? "0.2rem" : "0"} // Add margin-top for small screens
+        >
+          
+          <VStack gap={3} alignItems="flex-start">
+          <Box p="1rem" borderRadius="8px" bg="#1a1a1a" border="solid 1px #5A5D9F" width="100%">
+          <Text size="lg">KEY RESULTS</Text>
+            </Box>
+            <Box p="1.5rem" borderRadius="8px" bg="#1a1a1a" border="solid 1px #5A5D9F">
+              <Heading size="sm" mb="4px">Content Engagement</Heading>
+              <Flex direction="row" gap={2} alignItems="center">
+                <Image src={MendeleyEngagement} maxW="50%" />
+                <Text size="md">4.5 / 5</Text>
+              </Flex>
+            </Box>
+            <Box p="1.5rem" borderRadius="8px" bg="#1a1a1a" border="solid 1px #5A5D9F">
+              <Heading size="sm" mb="4px">Perception of Trust</Heading>
+              <Flex direction="row" gap={2} alignItems="center">
+                <Image src={MendeleyTrust} maxW="50%" />
+                <Text size="md">4.4 / 5</Text>
+              </Flex>
+            </Box>
+            <Box p="1.5rem" borderRadius="8px" bg="#1a1a1a" border="solid 1px #5A5D9F">
+              <Heading size="sm" mb="4px">Content Clarity</Heading>
+              <Flex direction="row" gap={2} alignItems="center">
+                <Image src={MendeleyClarity} maxW="50%" />
+                <Text size="md">4.5 / 5</Text>
+              </Flex>
+            </Box>
+          </VStack>
+         
+        </Box>
+      </Box>
+    </Box>
+  
+  
+  );
+};
 
 
-
-
-          </Box>
-
-
-        </Flex>
-  </Box>
- 
- 
-);
 
 export default NewHomePagePreview;
--- src/components/project-page-components/project-header.jsx
+++ src/components/project-page-components/project-header.jsx
@@ -1,35 +1,61 @@
-import React from "react"
-import { Stack, Box, Text, Heading, Image, Container, Flex, GridItem } from '@chakra-ui/react'
+import React from "react";
+import { Stack, Box, Text, Heading, Image, Container, Flex, GridItem } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
-
-const ProjectHeader = ({ title, subtitle, bg, img, headingColor, headingGradient, imgMaxWidth, imgPadding }) => (
-  <Box bg={bg} >
-    <Container maxW="1300px" >
-      <Flex mx="auto" pt={{base: '8rem', md: "11rem"}} 
-      // pb={{base: '12rem', md: "22rem"}} 
-      alignItems="center" 
-      justifyItems="center" 
-      direction="column" 
-      width="100%" position="relative">
-        {/* <Heading size={{ base: '2xl', md: '3xl' }} textAlign="center">{title}</Heading> */}
-        <Heading
-          size={{ base: '2xl', md: '3xl' }}
-          textAlign="center"
-          bgGradient={headingGradient || 'none'}
-          bgClip={headingGradient ? 'text' : 'initial'}
-          color={headingGradient ? 'transparent' : headingColor}
+const ProjectHeader = ({ title, subtitle, bg, img, headingColor, headingGradient, imgMaxWidth, imgPadding }) => {
+  return (
+    <Box bg={bg}>
+      <Container maxW="1300px">
+        <Flex
+          mx="auto"
+          pt={{ base: '8rem', md: '11rem' }}
+          alignItems="center"
+          justifyItems="center"
+          direction="column"
+          width="100%"
+          position="relative"
         >
-          {title}
-        </Heading>
-        <Text size="xl" mt={2} textAlign="center">{subtitle} </Text>
-        <Image pt="10rem" src={img} borderRadius="12px 12px 0 0" maxWidth={imgMaxWidth} height="auto" width="100%" 
-        padding={imgPadding}
-         />
-
-      </Flex>
-    </Container>
-  </Box>
-
-);
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <Heading
+              size={{ base: '2xl', md: '3xl' }}
+              textAlign="center"
+              bgGradient={headingGradient || 'none'}
+              bgClip={headingGradient ? 'text' : 'initial'}
+              color={headingGradient ? 'transparent' : headingColor}
+            >
+              {title}
+            </Heading>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.3 }}
+          >
+            <Text fontSize="xl" mt={2} textAlign="center">{subtitle}</Text>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.6  }} 
+          >
+            <Image
+              pt="10rem"
+              src={img}
+              borderRadius="12px 12px 0 0"
+              maxWidth={imgMaxWidth}
+              height="auto"
+              width="100%"
+              padding={imgPadding}
+            />
+          </motion.div>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
 
 export default ProjectHeader;
