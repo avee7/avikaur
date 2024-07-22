5c5790fb61aac2d08fa1d42ef60a8f9072abea96
avee7 <kauravee101@gmail.com>
2024-07-22 15:56:35 +0100

    Summary updated

--- src/components/hero-area.jsx
+++ src/components/hero-area.jsx
@@ -15,7 +15,7 @@ const HeroArea = () => {
   const opacityTransform = useTransform(scrollY, [0, 300], [1, 0.5]);
 
   return (
-    <MotionFlex zIndex="1" width="100%" p={{ base: "10rem 0 4rem", lg: "16rem 0 10rem" }} m={{ base: "0 auto" }} direction="row" position="relative" gap="2rem" height="80vh">
+    <MotionFlex zIndex="1" width="100%" p={{ base: "10rem 0 4rem", lg: "16rem 0 10rem" }} m={{ base: "0 auto" }} direction="row" position="relative" gap="2rem" height="80vh" justifyContent="center">
       <MotionBox
         position="absolute" width='100%' background="rgba(90, 93, 159, 0.18)" zIndex="-1"
         backdropFilter="blur(5px)" height="100%" top="0" left="0"
@@ -24,36 +24,45 @@ const HeroArea = () => {
         transition={{ duration: 0.5, delay: 0.5 }}
       />
 
-      <Stack direction="column" position="relative" gap="2rem" maxW="1300px" mx="auto">
+      <Stack direction="column" position="relative" gap="2rem" maxW="800px">
       <motion.div
-        style={{ y: yTransform, scale: scaleTransform, opacity: opacityTransform, maxWidth: '1300px', zIndex: '1', position: 'relative', margin: '0 auto', width: '100%' }}
+        style={{ y: yTransform, scale: scaleTransform, opacity: opacityTransform, 
+          // maxWidth: '1300px', 
+          zIndex: '1', 
+          position: 'relative', 
+          // margin: '0 auto', 
+          width: '100%' }}
         initial={{ y: 20, opacity: 0, visibility: 'hidden' }}
         animate={{ y: 0, opacity: 1, visibility: 'visible' }}
         transition={{ delay: 0.8, duration: 0.9 }}
       >
-        {/* <Box fontSize="3rem" p={{ base: "0 2rem 1rem" }}>&#128075;&#127996;</Box> */}
+        
         <MotionHeading
           as="h1"
           p={{ base: "0 2rem 1rem", xl: "0 0 1rem" }}
-
+          textAlign="center"
           size={{ base: '3xl', md: '4xl' }}
           background="linear-gradient(263deg, #bba6ff 0%, #6c63ff 100%)"
           backgroundClip="text"
         >
           Avinash Kaur
         </MotionHeading>
+        {/* <Box fontSize="2rem" p={{ base: "0 0 1rem" }} float='left'>&#128075;&#127996;</Box> */}
         <MotionText position="relative" zIndex="1"
         initial={{ y: 20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 1.25, duration: 0.9 }}
-        size={{ base: '2xl', md: '3xl' }}
+        textAlign="center"
+        size={{ base: '2xl', md: 'xl' }}
         width="100%"
         p={{ base: "0 2rem", xl: "0" }}
-        maxW="1300px"
         mx="auto"
         
       >
-        London-based Product Designer with over 7 years of experience crafting engaging, scalable interfaces.
+        
+        {/* I'm a product designer with a background in front-end development, passionate about building engaging, scalable, and purposeful digital experiences. */}
+        I'm a product designer who integrates design passion with front-end development skills to craft engaging, scalable, and purposeful digital experiences.
+        
       </MotionText>
       </motion.div>
 
